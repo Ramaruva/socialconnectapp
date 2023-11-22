a921240810ebe417491ac54523604abab5f70f9b
Ramaruva <ramaruva54@gmail.com>
2023-11-22 10:00:34 -0600

    basic authlayer setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-import './global.css'
+import './globals.css'
 import SignInForm from './_auth/forms/SignInForm'
 import SignUpForm from './_auth/forms/SignUpForm'
 import { Home } from './_root/pages/Index'
