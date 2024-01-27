a1d783df9e5b1b59671d2a8bbce1c837ffbcae66
Ramaruva <ramaruva54@gmail.com>
2024-01-27 16:00:57 -0600

    new setup of project

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
+import './globals.css';
+import SignInForm from './_auth/forms/SignInForm';
+import SignupForm from './_auth/forms/SignupForm';
+import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+
+const App = () => {
+    return (
+        <main className='flex h-screen'>
+            <Routes>
+                {/* public routes */}
+                <Route element={<AuthLayout />}>
+                    <Route path='/sign-in' element={<SignInForm />} />
+                    <Route path='/sign-in' element={<SignupForm />} />
+                </Route>
+                {/* private router */}
+                <Route element={<RootLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </main>
+    )
+}
+
+export default App
